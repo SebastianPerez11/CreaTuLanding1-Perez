fb8498cfcddc9a4b5560a9bfdb6805ee0266e659
SebastianPerez11 <sebassperez11@gmail.com>
2025-10-06 17:42:40 -0300

    se modificaron estilos css para adaptarlo a movil

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,10 +4,10 @@ import CartWidget from "./CartWidget";
 const Navbar = () => {
   return (
     <>
-      <nav className="bg-[#1a1a1a] p-10 text-white flex items-center justify-between">
+      <nav className="bg-[#1a1a1a] w-full max-xl:flex-col max-md:px-2 max-md:gap-4 p-10 text-white flex items-center justify-between">
         <h1 className="text-2xl font-medium font-serif">NOIR ESSENCE</h1>
-        <header className="flex gap-10 items-center">
-          <ul className="flex gap-4">
+        <header className="flex gap-10 items-center max-md:flex-col max-md:gap-6">
+          <ul className="flex gap-4 max-md:gap-2">
             <LinkNavBar text={"Inicio"} />
             <LinkNavBar text={"Catálogo"} />
             <LinkNavBar text={"Hombre"} />
